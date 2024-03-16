84bf153bf12f8e773e3640244f1c31c10c52e07a
e-quill <e-quill@users.noreply.github.com>
2024-03-16 21:22:53 +0530

    Color Boxes

--- js-file.js
+++ js-file.js
@@ -3,6 +3,9 @@ function createGrid(){
     for (j = 0; j < 16; j++){
       const container = document.querySelector(".container");
       const div = document.createElement("div");
+      div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "black";
+      });
       div.classList.add("box");
       container.appendChild(div);
     }
