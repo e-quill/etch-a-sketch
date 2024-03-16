3ed79222a9622dbe239ec202cf10f3305b180a8a
e-quill <e-quill@users.noreply.github.com>
2024-03-16 22:16:06 +0530

    Add buttons for dimensions and reset

--- js-file.js
+++ js-file.js
@@ -1,6 +1,6 @@
-function createGrid(){
-  for (i = 0; i < 16; i++){
-    for (j = 0; j < 16; j++){
+function createGrid(squares = 16){
+  for (i = 0; i < squares; i++){
+    for (j = 0; j < squares; j++){
       const container = document.querySelector(".container");
       const div = document.createElement("div");
       div.addEventListener("mouseover", () => {
@@ -10,6 +10,19 @@ function createGrid(){
       container.appendChild(div);
     }
   }
+}
+
+function promptDimensions() {
+  let input = prompt("Number of Square Per Side (Maximum: 100)")
+  createGrid(input)
+}
+
+function resetGrid() {
+  const elements = document.querySelectorAll('.box');
+
+  elements.forEach(element => {
+  element.style.backgroundColor = 'white';
+  });
 }
 
 createGrid()
